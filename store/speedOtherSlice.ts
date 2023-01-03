65a78a8c9688390cea229bb10dc785f538abb736
momo-dev1 <didy1234567@gmail.com>
2023-01-03 17:11:38 +0200

    rafactoring

--- store/speedOtherSlice.ts
+++ store/speedOtherSlice.ts
@@ -6,46 +6,50 @@ const initialState = {
     amount: 0,
     speedOther: SpeedOther,
     resourcePack: ResourcesData,
-}
+};
 
 const speedOtherSlice = createSlice({
-    name: 'speedOther',
+    name: "speedOther",
     initialState,
     reducers: {
         clearValues: (state) => {
-            return initialState
+            return initialState;
         },
         setAmount: (state, { payload: { name, value } }) => {
-            if (state.speedOther.find(item => item.name.includes(name))) {
-                let idx = state.speedOther.findIndex(item => item.name.includes(name))
-                state.speedOther[idx].value = +value
-            } else if (state.resourcePack.find(item => item.name.includes(name))) {
-                let idx = state.resourcePack.findIndex(item => item.name.includes(name))
-                state.resourcePack[idx].value = +value
+            const speedOtherIndex = state.speedOther.findIndex(
+                (item) => item.name.includes(name)
+            );
+            const resourcePackIndex = state.resourcePack.findIndex(
+                (item) => item.name.includes(name)
+            );
+
+            if (speedOtherIndex !== -1) {
+                state.speedOther[speedOtherIndex].value = +value;
+            } else if (resourcePackIndex !== -1) {
+                state.resourcePack[resourcePackIndex].value = +value;
             }
         },
         sumCount: (state) => {
-            const resourceTotal = (tier: any[]) => {
+            const resourceTotal = (tier: any) => {
                 return tier.map((item: any) => item).reduce((acc: any, curr: any) => {
-                    let { amount, value } = curr
-                    let totalAmount = amount * value
-                    acc.amount += totalAmount
-                    return acc
+                    let { amount, value } = curr;
+                    let totalAmount = amount * value;
+                    acc.amount += totalAmount;
+                    return acc;
                 }, { amount: 0 });
-            }
-            const speedOther = resourceTotal(state.speedOther)
-            const resourcePack = resourceTotal(state.resourcePack)
+            };
 
-            if (state.speedOther.some(item => item.value > 0)) {
-                state.amount = speedOther.amount
+            const speedOther = resourceTotal(state.speedOther);
+            const resourcePack = resourceTotal(state.resourcePack);
+
+            if (state.speedOther.some((item) => item.value > 0)) {
+                state.amount = speedOther.amount;
             } else {
-                state.amount = resourcePack.amount
+                state.amount = resourcePack.amount;
             }
-
-        }
-    }
-})
-
+        },
+    },
+});
 
 export const { sumCount, setAmount, clearValues } = speedOtherSlice.actions;
 
